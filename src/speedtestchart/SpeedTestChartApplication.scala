299e4f8a8c585d079bdffdfa20d7307945e16cf9
Nicola Ng <ngnicola513@gmail.com>
2015-04-10 17:17:23 +0800

    test

--- src/speedtestchart/SpeedTestChartApplication.scala
+++ src/speedtestchart/SpeedTestChartApplication.scala
@@ -11,8 +11,6 @@ import speedtestchart.fetcher.Fetcher
 import scala.swing.SimpleSwingApplication
 import scala.util.Random
 import scala.util.matching.Regex
-import scalafx.application.JFXApp
-
 
 
 /**
@@ -28,67 +26,6 @@ object SpeedTestChartApplication extends Thread {
   }
 }
 
-private object SpeedTestChartScalaFxApplication extends JFXApp {
-  val random = new Random(System.currentTimeMillis())
-
-  import scalafx.application.JFXApp.PrimaryStage
-  import scalafx.scene._
-  import scalafx.scene.effect.BoxBlur
-  import scalafx.scene.paint.Color
-  import scalafx.scene.paint.Color._
-  import scalafx.scene.shape.Circle
-
-  stage = new PrimaryStage {
-    title = "SpeedTest Chart ScalaFx Application"
-    width = 800
-    height = 600
-    scene = new Scene {
-      fill = Color.WHEAT
-      content = for (i <- 0 until 50) yield new Circle {
-        centerX = random.nextInt(800)
-        centerY = random.nextInt(800)
-        radius=150
-        fill=color(random.nextDouble(),random.nextDouble(),random.nextDouble(),0.2)
-        effect=new BoxBlur(10,10,3)
-      }
-    }
-    //scene=new Scene{FXMLLoader.load(getClass.getResource("/speedtestchart/gui/MyView.fxml"))}
-  }
-}
-
-import javafx.application.Application
-private class SpeedTestChartJavaFxApplication extends Application with Runnable {
-  import javafx.scene.Scene
-  import javafx.scene.control.{Button, Label}
-  import javafx.scene.layout.{FlowPane, StackPane}
-  import javafx.stage.Stage
-
-  override def start(stage: Stage): Unit = {
-    println("loading FX stage")
-    stage.setTitle("SpeedTest Chart JavaFxApplication")
-    val root = new StackPane()
-    val loading = new Label("loading")
-    //root.getChildren.add(loading)
-    val hk = new Button("Speedtest.cn (Mainland)")
-
-    val mainland = new Button("Speedtest.com (Hong Kong)")
-    root.getChildren.add(hk)
-    root.getChildren.add(mainland)
-    val panel = new FlowPane
-    panel.getChildren.add(hk)
-    panel.getChildren.add(mainland)
-    stage.setScene(new Scene(panel, 600, 450))
-    root.getChildren.remove(loading)
-    stage.show()
-  }
-
-
-
-  override def run(): Unit = {
-    println("opening gui window")
-    Application.launch(classOf[SpeedTestChartJavaFxApplication])
-  }
-}
 
 private class SpeedTestChartApplicationRunnable extends SimpleSwingApplication with Runnable {
 
@@ -103,17 +40,18 @@ private class SpeedTestChartApplicationRunnable extends SimpleSwingApplication w
   var path: String = _
   var regex: String = _
 
-  def readFile(fetcher: Fetcher) :Unit = {
-    fetcher.getFiles.foreach(f=> readFile(f) )
+  def readFile(fetcher: Fetcher): Unit = {
+    fetcher.getFiles.foreach(f => readFile(f))
   }
-  def readFile(file:File): Unit ={
-    val in=new FileInputStream(file)
-    if(in.available()<1)return
+
+  def readFile(file: File): Unit = {
+    val in = new FileInputStream(file)
+    if (in.available() < 1) return
     /*val array=Array.fill[Byte](in.available())(0)
     in.read(array)
     array.foreach(b=>print(b.toChar))*/
-    val reader=new BufferedReader(new InputStreamReader(in))
-    val lines=reader.lines()
+    val reader = new BufferedReader(new InputStreamReader(in))
+    val lines = reader.lines()
 
     println()
   }
@@ -121,8 +59,8 @@ private class SpeedTestChartApplicationRunnable extends SimpleSwingApplication w
   def init = {
     fetcher = new Fetcher(path, new Regex(regex))
     updateView
-    val result=Dialog.showConfirmation(null,"start?","confirm",Dialog.Options.OkCancel,Dialog.Message.Question,null)
-    if(result.eq(Dialog.Result.Ok)){
+    val result = Dialog.showConfirmation(null, "start?", "confirm", Dialog.Options.OkCancel, Dialog.Message.Question, null)
+    if (result.eq(Dialog.Result.Ok)) {
       readFile(fetcher);
     }
   }
